419f786f14d7d830b464670e01fd7b502fa101d7
w.dehai <342252328@qq.com>
2018-11-02 14:01:23 +0800

    22

--- src/main/resources/conf/app.sh
+++ src/main/resources/conf/app.sh
@@ -73,50 +73,51 @@ function checkStart() {
       }
       
       
-      function start_canal() {
-          echo "start canal ..."
-          echo "===>>>><<<<==="
-          serverPort=`perl -le 'print $ENV{"canal.port"}'`
-          if [ -z "$serverPort" ] ; then
-              serverPort=11111
-          fi
-      
-          destination=`perl -le 'print $ENV{"canal.destinations"}'`
-          if [[ "$destination" =~ ',' ]]; then
-              echo "multi destination:$destination is not support"
-              exit 1;
-          else
-              if [ "$destination" != "" ] && [ "$destination" != "example" ] ; then
-                  if [ -d /home/admin/canal-server/conf/example ]; then
-                      mv /home/admin/canal-server/conf/example /home/admin/canal-server/conf/$destination
-                  fi
+  function start_canal() {
+      echo "start canal ..."
+      serverPort=`perl -le 'print $ENV{"canal.port"}'`
+      if [ -z "$serverPort" ] ; then
+          serverPort=11111
+      fi
+  
+      destination=`perl -le 'print $ENV{"canal.destinations"}'`
+      echo "~~~~~~~~~~"
+      echo $destination 
+      if [[ "$destination" =~ ',' ]]; then
+          echo "multi destination:$destination is not support"
+          exit 1;
+      else
+          if [ "$destination" != "" ] && [ "$destination" != "example" ] ; then
+              if [ -d /home/admin/canal-server/conf/example ]; then
+                  mv /home/admin/canal-server/conf/example /home/admin/canal-server/conf/$destination
               fi
           fi
-          #su admin -c 'cd /home/admin/canal-server/bin/ && sh restart.sh 1>>/tmp/start.log 2>&1'
-          #sleep 5
-          #check start
-          #checkStart "canal" "nc 127.0.0.1 $serverPort -w 1 -z | wc -l" 30
-      }
-     
-     function stop_canal() {
-         echo "stop canal"
-         su admin -c 'cd /home/admin/canal-server/bin/ && sh stop.sh 1>>/tmp/start.log 2>&1'
-         echo "stop canal successful ..."
-     }
-     
-     echo "==> START ..."
-     echo "^^^^^^^^^"
-     
-     start_canal
-     
-     echo "==> START SUCCESSFUL ..."
-     
-     tail -f /dev/null &
-     # wait TERM signal
-     waitterm
-     
-     echo "==> STOP"
-     
-     stop_canal
-     
-     echo "==> STOP SUCCESSFUL ..."
+      fi
+      echo "^^^^^^^^^^^^^"
+      su admin -c 'cd /home/admin/canal-server/bin/ && sh restart.sh 1>>/tmp/start.log 2>&1'
+      sleep 5
+      #check start
+      checkStart "canal" "nc 127.0.0.1 $serverPort -w 1 -z | wc -l" 30
+  }
+ 
+ function stop_canal() {
+     echo "stop canal"
+     su admin -c 'cd /home/admin/canal-server/bin/ && sh stop.sh 1>>/tmp/start.log 2>&1'
+     echo "stop canal successful ..."
+ }
+ 
+ echo "==> START ..."
+ 
+ start_canal
+ 
+ echo "==> START SUCCESSFUL ..."
+ 
+ tail -f /dev/null &
+ # wait TERM signal
+ waitterm
+ 
+ echo "==> STOP"
+ 
+ stop_canal
+ 
+ echo "==> STOP SUCCESSFUL ..."
