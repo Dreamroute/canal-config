e8ecb380625880a962769fbd273ef6ed607ff161
w.dehai <342242328@qq.com>
2018-11-06 10:32:54 +0800

    123

--- src/main/resources/conf/app.sh
+++ src/main/resources/conf/app.sh
@@ -101,7 +101,7 @@ function start_canal() {
     
     echo $serverPort "   " $destination  
     
-    su -admin -c 'cd /home/admin/canal-server/bin/ && sh restart.sh 1>>/tmp/start.log 2>&1'
+    su admin -c 'cd /home/admin/canal-server/bin/ && sh restart.sh 1>>/tmp/start.log 2>&1'
     sleep 5
     #check start
     checkStart "canal" "nc 127.0.0.1 $serverPort -w 1 -z | wc -l" 30
